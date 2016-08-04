f99e1bc43d251ec9effe23723a72dcd3957ac05d
x0x1x2 <avicode@yahoo.com>
2016-08-05 00:31:29 +0300

    first tcp version

--- app/src/main/java/com/avicode/myapp/MyActivity.java
+++ app/src/main/java/com/avicode/myapp/MyActivity.java
@@ -16,6 +16,7 @@ import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
@@ -26,19 +27,16 @@ import java.net.Socket;
 import java.nio.charset.Charset;
 
 public class MyActivity extends AppCompatActivity {
+    private final String tag = "YS" +  " MyActivity";
+    public final static String EXTRA_MESSAGE = "com.avicode.myapp.MESSAGE";//UI
 
-    public final static String EXTRA_MESSAGE = "com.avicode.myapp.MESSAGE";
+    //public final static String serverAddr = "192.168.1.108"; // server address
+    public final static String serverAddr = "192.168.1.118"; // server address
+    public final static int serverPort = 25810; // server port
 
-    public final static String serverAddr = "192.168.1.119";
-    public final static int serverPort = 25810;
-
-    public final static String addr = "127.0.0.1";
-
-    public final static int port = 25800;
-    private DatagramSocket socket;
+    private Socket socket=null;
     private RecvTask recvTask;
     private Handler recvHandler = new Handler();
-    private String remoteAddr = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,75 +55,31 @@ public class MyActivity extends AppCompatActivity {
         });
 
 
-        //start receive thread
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    //init recv
-                    InetAddress laddr = InetAddress.getByName(addr);
-                    Log.e("YS", "begin:" + laddr.getHostAddress());
-
-                    //socket = new DatagramSocket(port,InetAddress.getByName("localhost"));
-                    socket = new DatagramSocket(port);
-
-                    recvTask = new RecvTask(socket,recvHandler);
-                    Log.e("YS", "end:" +  laddr.getHostName());
-                }catch (Exception ex) {
-                    ex.printStackTrace();
-                    Log.e("YS", "onCreate exception" + ex.getMessage());
-                }
-
-            }
-        }).start();
-
         //connect to server
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
                     do {
-
-
-                        InetAddress addr = InetAddress.getByName(serverAddr);
-
-                        Socket socket = new Socket(addr, serverPort);
-                        Log.e("YS", "connected to server: " + serverAddr + ':' + serverPort);
-                        String regMsgStr = "REG";
-                        char[] regMsgData = regMsgStr.toCharArray();
-                        //socket.getOutputStream().write(regMsgData);
-//                    socket.getOutputStream().flush();
-//                    socket.getInputStream().wait();
-
-                        PrintWriter bufferOut = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-                        bufferOut.write(regMsgData);
-                        bufferOut.flush();
-                        Thread.sleep(200,0);
-                        BufferedReader bufferIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                        char[] recvMsgData = new char[128];
-                        bufferIn.read(recvMsgData, 0, 128);
-
-                        String recvMsgStr = new String(recvMsgData);
-                        String[] clientList = recvMsgStr.split("\n");
-                        int token = recvMsgStr.indexOf('\n');
-
-
-                        Log.e("YS", "RECV from server:" + recvMsgStr);
-
-                        if (clientList.length > 1 && clientList[1] != null) {
-                            remoteAddr = clientList[1];
+                        InetAddress lAddr = InetAddress.getByName(serverAddr);
+                        Log.e(tag, "begin:" + lAddr.getHostAddress());
+                        try {
+                            //Create socket
+                            socket = new Socket(lAddr, serverPort);
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                            Log.e(tag, "onCreate socket exception " + ex.getMessage());
                         }
-                        bufferOut.close();
-                        bufferIn.close();
-                        socket.close();
-                        Thread.sleep(10*1000,0);
+                        Log.e(tag, "end:" + lAddr.getHostName());
+                    } while(socket == null);
 
-                    }while(remoteAddr=="");
+                    //create receive thread
+                    recvTask = new RecvTask(socket, recvHandler);
+
                 } catch (Exception ex) {
                     ex.printStackTrace();
-                    Log.e("YS", "onCreate server connection exception " + ex.getMessage());
+                    Log.e(tag, "onCreate server connection exception " + ex.getMessage());
                 }
-
             }
         }).start();
 
@@ -159,23 +113,30 @@ public class MyActivity extends AppCompatActivity {
         textView.append("\n" + src +": "+message);
 
     }
-    private void sendMessage(final String message, final String remoteIp ){
+    private void sendMessage(final String message){
 
+        if(socket == null ) {
+            Log.e(tag, "sendMessage: no connection to server" );
+            return;
+        }
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    byte[] buf = message.getBytes("UTF-8");
-                    Log.e("YS", "begin send message:" + message);
-                    DatagramPacket packet = new DatagramPacket(buf ,buf.length ,InetAddress.getByName(remoteIp), port);
+                    //byte[] buf = message.getBytes("UTF-8");
+                    Log.e(tag, "begin send message:" + message);
 
-                    socket.send(packet);
+                    BufferedWriter bufferOut = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-                    Log.e("YS", "done send message:" + new String(packet.getData(), 0, packet.getLength()));
+                    bufferOut.write(message);
+                    bufferOut.flush();
+                    //bufferOut.close();
+
+                    Log.e(tag, "done send message:");
                 }catch (Exception ex)
                 {
                     ex.printStackTrace();
-                    Log.e("YS","sendMessage exception" + ex.getMessage() );
+                    Log.e(tag,"sendMessage exception" + ex.getMessage() );
                 }
 
             }
@@ -195,8 +156,8 @@ public class MyActivity extends AppCompatActivity {
         //print
         echoMessage("me", message);
         //send to remote side
-        if(remoteAddr != "")
-        sendMessage(message,remoteAddr);
+
+        sendMessage(message);
 
 //        Intent intent = new Intent(this, DisplayMessageActivity.class);
 //        intent.putExtra(EXTRA_MESSAGE, message);
@@ -217,8 +178,9 @@ public class MyActivity extends AppCompatActivity {
         }
     }
     public class RecvTask {
+        private final String tag = "YS" +  "RecvTask: ";
         private final static int MESSAGE_SIZE = 1500;
-        private DatagramSocket socket;
+        private Socket socket;
         private boolean runListener;
         private Thread recvTread;
         private Handler recvHandler;
@@ -226,59 +188,49 @@ public class MyActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    byte[] recvData = new byte[MESSAGE_SIZE];
-//                    InetAddress laddr = InetAddress.getByName("172.0.0.1");
-//                    Log.e("YS", laddr.getHostAddress());
-//                    Log.e("YS", laddr.getHostName());
-                    Log.e("YS", "starting RecvTask");
+
+                    Log.e(tag, "starting RecvTask");
+                    BufferedReader bufferIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    char[] recvMsgData = new char[MESSAGE_SIZE];
+
                     while(runListener){
+                         int len=0;
+                        try {
+                            len = bufferIn.read(recvMsgData, 0, MESSAGE_SIZE);
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                            Log.e(tag, "buffer read exception " + ex.getMessage());
+                        }
 
-                        DatagramPacket datagramPacket = new DatagramPacket(recvData,MESSAGE_SIZE);
-                        socket.receive(datagramPacket);
+                        String recvMsgStr = new String(recvMsgData, 0, len);
+                        String[] msgList = recvMsgStr.split("\0");
+                        Log.e(tag, "RECV from server:" + recvMsgStr);
 
-                        String recvMsg = new String(datagramPacket.getData(), 0, datagramPacket.getLength());
-                        Log.e("YS","RecvTask: posting msg:" +  recvMsg  );
-
-                        recvHandler.post(new MyActivity.UpdateChat(recvMsg));
-                        Log.e("YS","RecvTask: posting msg: done!" +  recvMsg  );
+                        recvHandler.post(new MyActivity.UpdateChat(msgList[0]));
+                        Log.e(tag," posting msg: done!" +  msgList[0]);
 
                     }
                 }catch (Exception ex){
                     ex.printStackTrace();
-                    Log.e("YS", "RecvTask:"+ex.getMessage() );
+                    Log.e(tag, "Exception "+ex.getMessage() );
                 }
             }
         };
 
-        public RecvTask(int port, String address){
+        public RecvTask(Socket socket, Handler recvHandler){
             try {
-
-                InetAddress laddr = InetAddress.getByName(address);
-                socket = new DatagramSocket(port, laddr);
-
-                recvTread = new Thread(runnableThread);
-                runListener = true;
-                recvTread.start();
-
-
-            }catch (Exception ex){
-                ex.printStackTrace();
-                Log.e("YS", ex.getMessage() );
-            }
-        }
-
-        public RecvTask(DatagramSocket socket, Handler recvHandler){
-            try {
+                //Save data
                 this.socket = socket;
                 this.recvHandler = recvHandler;
-                recvTread = new Thread(runnableThread);
-                runListener = true;
-                recvTread.start();
+                //Receive thread
+                recvTread = new Thread(runnableThread); // creat
+                runListener = true;  // set flag true
+                recvTread.start(); // start it
 
 
             }catch (Exception ex){
                 ex.printStackTrace();
-                Log.e("YS","RecvTask ctor" +ex.getMessage() );
+                Log.e(tag,"c'tor Exception " +ex.getMessage() );
             }
         }
 
